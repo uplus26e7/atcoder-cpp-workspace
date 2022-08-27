cd212654daf43518133603ddc4265a0a05b037bd
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2022-08-27 11:31:56 +0000

    ✅ ABC085/C

--- contests/abc085/c/main.cpp
+++ contests/abc085/c/main.cpp
@@ -3,4 +3,26 @@ using namespace std;
 
 int main()
 {
+    int N, Y;
+    cin >> N >> Y;
+    int ares = -1, bres = -1, cres = -1;
+    for (int a = 0; a <= N; a++)
+    {
+        for (int b = 0; b <= N; b++)
+        {
+            int c = N - a - b;
+            if (c < 0 || c > N)
+            {
+                continue;
+            }
+
+            if (10000 * a + 5000 * b + 1000 * c == Y)
+            {
+                ares = a;
+                bres = b;
+                cres = c;
+            }
+        }
+    }
+    cout << ares << " " << bres << " " << cres << endl;
 }
