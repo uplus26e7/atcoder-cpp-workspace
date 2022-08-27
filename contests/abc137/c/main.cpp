1a3390bfa585da48e1d85881c186029448b129e7
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2022-08-27 11:47:43 +0000

    ✅ ABC137/C

--- contests/abc137/c/main.cpp
+++ contests/abc137/c/main.cpp
@@ -3,4 +3,22 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    map<string, long long> num;
+    for (int i = 0; i < N; i++)
+    {
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        num[s]++;
+    }
+
+    long long result = 0;
+    for (auto v : num)
+    {
+        long long n = v.second;
+        result += n * (n - 1) / 2;
+    }
+    cout << result << endl;
 }
