9948fe33fb3a880cff3f23ef6bbec0dd34d92bae
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2022-08-27 11:40:02 +0000

    ✅ ABC085/B

--- contests/abc085/b/main.cpp
+++ contests/abc085/b/main.cpp
@@ -3,4 +3,18 @@ using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+
+    set<int> S;
+    for (int i = 0; i < N; i++)
+    {
+        int d;
+        cin >> d;
+        if (S.count(d) == 0)
+        {
+            S.insert(d);
+        }
+    }
+    cout << S.size() << endl;
 }
